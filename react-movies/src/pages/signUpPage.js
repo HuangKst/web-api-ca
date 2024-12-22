6ea23df6d9983edde7d95b68a727a710238060f2
HuangKst <1300105758@qq.com>
2024-12-22 17:30:13 +0000

    Beautify the interface

--- react-movies/src/pages/signUpPage.js
+++ react-movies/src/pages/signUpPage.js
@@ -1,6 +1,14 @@
 import React, { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/authContext";
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Alert,
+} from "@mui/material";
 
 const SignUpPage = () => {
   const context = useContext(AuthContext);
@@ -39,37 +47,74 @@ const SignUpPage = () => {
   }
 
   return (
-    <>
-      <h2>Sign Up Page</h2>
-      <p>
-        You must register a username and password to log in. <br />
-        <strong>Password Requirements:</strong> At least 8 characters, include
-        one letter, one number, and one special character.
-      </p>
-      <input
-        value={userName}
-        placeholder="User name"
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <br />
-      <input
-        value={password}
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br />
-      <input
-        value={passwordAgain}
-        type="password"
-        placeholder="Password again"
-        onChange={(e) => setPasswordAgain(e.target.value)}
-      />
-      <br />
-      {/* Display error message */}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <button onClick={register}>Register</button>
-    </>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5" gutterBottom>
+          Sign Up
+        </Typography>
+        <Typography variant="body2" color="textSecondary" align="center" gutterBottom>
+          You must register a username and password to log in.
+        </Typography>
+        <Typography variant="body2" color="textSecondary" align="center" mb={2}>
+          <strong>Password Requirements:</strong> At least 8 characters, include one letter, one number, and one special character.
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1, width: "100%" }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="User Name"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="passwordAgain"
+            label="Confirm Password"
+            type="password"
+            id="passwordAgain"
+            autoComplete="current-password"
+            value={passwordAgain}
+            onChange={(e) => setPasswordAgain(e.target.value)}
+          />
+          {error && <Alert severity="error" sx={{ mt: 2 }}>{error}</Alert>}
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={register}
+          >
+            Register
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
 
