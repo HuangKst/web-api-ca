160726e428078cc4aba6b49139781492331759bc
HuangKst <1300105758@qq.com>
2024-12-25 13:13:47 +0000

    Add the upcoming api for front end

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -28,6 +28,29 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(returnObject);
 }));
 
+// router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+//     const upcomingMovies = await getUpcomingMovies();
+//     res.status(200).json(upcomingMovies);
+// }));
+// GET /api/movies/upcoming?page=2
+router.get('/upcoming', asyncHandler(async (req, res) => {
+    let { page = 1 } = req.query;
+    page = parseInt(page, 10);
+  
+    if (isNaN(page) || page < 1) {
+      return res
+        .status(400)
+        .json({ message: 'Invalid page number. Must be a positive integer.' });
+    }
+  
+    try {
+      const data = await getUpcomingMovies(page);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error('Error fetching upcoming movies:', error.message);
+      res.status(500).json({ message: 'Failed to fetch upcoming movies' });
+    }
+  }));
 
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
@@ -40,16 +63,15 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
-router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-    const upcomingMovies = await getUpcomingMovies();
-    res.status(200).json(upcomingMovies);
-}));
+
+
+  
 
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
     try {
         // 调用 getGenres 获取类型数据
         const genresData = await getGenres();
-        const genres = genresData.genres; // 提取 genres 字段
+        const genres = genresData.genres; 
         res.status(200).json(genres); // 返回 genres 列表
     } catch (error) {
         console.error("Error fetching genres:", error.message);
--- movies-api/index.js
+++ movies-api/index.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/movies', authenticate, moviesRouter);
-
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
